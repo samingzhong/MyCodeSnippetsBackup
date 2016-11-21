1bddf604d5f029cf02d6920f3c7f451219d0095c
钟晓明 <samingzhong@qq.com>
2016-11-21 17:01:42 +0800

    first commit with 4 codesnippet.

---
+++ copyToXcodeCodeSnippetsPath.sh
@@ -0,0 +1,3 @@
+echo "正拷贝当前目录的所有codesnippet文件到 \"~/Library/Developer/Xcode/UserData/CodeSnippets/\"..."
+cp ./*.codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets/
+echo "all things done!🍻"
